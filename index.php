378cb4c8948a0d7c4313acdabcb3fa81bb79fabb
whuwb <skywind2006@gmail.com>
2012-11-29 10:25:16 +0800

    Complete Login Page

--- index.php
+++ index.php
@@ -1,27 +1,115 @@
-<?php
-	session_start();
+<!DOCTYPE html> 
+<html> 
+	<head> 
+		<title>My Page</title> 
+		<meta name="viewport" content="width=device-width, initial-scale=1"> 
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
+		<link rel="stylesheet" href="css/jqm-docs.css" />
+	</head> 
 
-	include_once( 'config.php' );
-	include_once( 'saetv2.ex.class.php' );
+	<body>
 
-	$o = new SaeTOAuthV2( WB_AKEY , WB_SKEY );
+		<!-- 
+			$TODO: how can I remove those fucking stupid duplicated secondary navigations?  
+			--- Consider write an jQuery plugin which could just refresh part of UI later.
+		-->
+		<!-- searchBook Page -->
+		<div data-role="page" class="type-interior" id="searchBook">
 
-	$code_url = $o->getAuthorizeURL( WB_CALLBACK_URL );
+			<div data-role="header" data-theme="e">
+				<h1>我的图书馆</h1>
+			</div>
 
-?>
+			<div data-role="content">	
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Bin's Demo</title>
-</head>
+				<div class="content-primary">
+					<div data-role="fieldcontain" >
+						<input type="search" name="search" id="search"/>
+					</div>	
+					<br />
+					<ul data-role="listview"  data-theme="c" id="searchBookList">
+					</ul>
+				</div><!--/content-primary -->		
 
-<body>
-	
-	<p>Welcome</p>
-	
-    <p><a href="<?=$code_url?>"><img src="weibo_login.png" title="点击进入授权页面" alt="点击进入授权页面" border="0" /></a></p>
+				<div class="content-secondary">
 
-</body>
+					<div data-role="collapsible" data-collapsed="true" data-theme="b" data-content-theme="d">
+
+							<h3>More in this section</h3>
+
+							<ul data-role="listview"  data-theme="c" data-dividertheme="d">
+								<li data-theme="a"><a href="#searchBook">搜索图书</a></li>
+								<li><a href="#myBook">我的图书</a></li>	
+								<li><a href="#myFriend">我的好友</a></li>	
+							</ul>
+					</div>
+				</div>				
+			</div><!-- /content -->
+
+		</div><!-- /page -->
+
+		<!-- searchBook Page -->
+		<div data-role="page" class="type-interior" id="myBook">
+
+			<div data-role="header" data-theme="e">
+				<h1>我的图书馆</h1>
+			</div>
+
+			<div data-role="content">	
+
+				<div class="content-primary">
+					<h2>MyBook Page</h2>
+				</div><!--/content-primary -->		
+
+				<div class="content-secondary">
+
+					<div data-role="collapsible" data-collapsed="true" data-theme="b" data-content-theme="d">
+
+							<h3>More in this section</h3>
+
+							<ul data-role="listview"  data-theme="c" data-dividertheme="d">
+								<li><a href="#searchBook">搜索图书</a></li>
+								<li data-theme="a"><a href="#myBook">我的图书</a></li>	
+								<li><a href="#myFriend">我的好友</a></li>	
+							</ul>
+					</div>
+				</div>				
+			</div><!-- /content -->
+
+		</div><!-- /page -->
+
+		<!-- myfriend Page -->
+		<div data-role="page" class="type-interior" id="myFriend">
+
+			<div data-role="header" data-theme="e">
+				<h1>我的图书馆</h1>
+			</div>
+
+			<div data-role="content">	
+
+				<div class="content-primary">
+					<h2>My Friends Page</h2>
+				</div><!--/content-primary -->		
+
+				<div class="content-secondary">
+
+					<div data-role="collapsible" data-collapsed="true" data-theme="b" data-content-theme="d">
+
+							<h3>More in this section</h3>
+
+							<ul data-role="listview"  data-theme="c" data-dividertheme="d">
+								<li><a href="#searchBook">搜索图书</a></li>
+								<li><a href="#myBook">我的图书</a></li>	
+								<li data-theme="a"><a href="#myFriend">我的好友</a></li>	
+							</ul>
+					</div>
+				</div>				
+			</div><!-- /content -->
+
+		</div><!-- /page -->
+
+		<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+		<script src="js/searchPage.js"></script>
+	</body>
 </html>
---
+++ login.php
@@ -0,0 +1,168 @@
+<?php
+	header("Content-Type: text/html; charset=utf-8");
+	
+	session_start();
+
+	include_once( 'config.php' );
+	include_once( 'saetv2.ex.class.php' );
+
+	$o = new SaeTOAuthV2( WB_AKEY , WB_SKEY );
+
+	$code_url = $o->getAuthorizeURL( WB_CALLBACK_URL );
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Book Share</title>
+		
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1"> 
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
+		<link rel="stylesheet" href="css/jqm-docs.css" />
+		
+		<style type="text/css">
+		
+			.HeaderLogo{
+				width:15%;
+				height:10%;
+				float:left; 
+				margin:2% 2% 2% 13%;
+			}
+			
+			.HeaderLogin{
+				width:15%;
+				height:10%;
+				float:right; 
+				margin:2% 20% 2% 4%;
+			}
+			
+			.BodyDiv{ 
+				margin:5% 0% 3% 0%;
+				width : 100%;
+				padding : 3% 0%;
+				background : #4682B4;
+			}
+			
+			h1{
+				color : white;
+				font-size : 180%;
+			}
+			
+			h2{
+				color : white;
+				font-size : 150%;
+			}
+			
+			.ShareDiv{
+				float:left;
+				width : 16%;
+				margin : 4% 3% 3% 18%;
+			}
+			
+			.EasyDiv{
+				float:left;
+				width : 16%;
+				margin : 4% 3% 3% 5%;
+			}
+			
+			.SafeDiv{
+				float:left;
+				width : 16%;
+				margin : 4% 15% 3% 3%;
+			}
+			
+			.ContentPara{
+				color : white;
+				font-size : 100%;
+			}
+			
+			.FooterDiv{
+				color : #BEBEBE;
+				font-size : 100%;
+			}
+			
+			.ContentImage{
+				width : 90%;
+			}
+			
+			.HeadImage{
+				width : 90%;
+			}
+			
+		</style>
+		
+	</head>
+
+	<body>
+		
+		<div data-role="page" >
+			
+			<div >
+				<div class = "HeaderLogo">
+					<img src="photo/bookShareLogo.png" class = "HeadImage" />
+				</div>
+				
+				
+				<div class = "HeaderLogin">
+					<a href="<?=$code_url?>"><img src="photo/weibo_login.png" title="点击进入授权页面" alt="点击进入授权页面"  class = "HeadImage" /></a>
+				</div>
+				
+				<br/>
+			</div>
+			
+			<div class="BodyDiv" align=center >
+				
+				<h1>BookShare，分享您的图书：</h1>
+				
+				<div class = "ShareDiv" >
+					
+					<img src="photo/easy.png" class = "ContentImage" />
+					
+					<h2>分享</h2>
+					
+					<p class = "ContentPara">					
+						和您的好友分享书籍、分享知识
+					</p>
+				</div>
+				
+				<div class = "EasyDiv">
+					<img src="photo/easy.png" class = "ContentImage" />
+					
+					<h2>便捷</h2>
+					
+					<p class = "ContentPara">					
+						任何时间、任何地点、任何终端，一样的服务体验
+					</p>
+				</div>
+				
+				<div class = "SafeDiv">
+					<img src="photo/easy.png" class = "ContentImage" />
+					
+					<h2>安全</h2>
+					
+					<p class = "ContentPara">					
+						只有您的好友，才能和您进行分享
+					</p>
+				</div>
+				
+			</div><!--/content-primary -->		
+			
+			<div class = "FooterDiv" align=center>
+				<p>Copyright @ who cares . All rights reserved.</p>
+			</div>
+			
+		</div><!-- /page -->
+		
+		<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+		<script src="js/searchPage.js"></script>
+		
+		<script>
+
+			$(".BodyDiv").height( $(window).height() * 0.7 );
+
+		</script>
+	</body>
+</html>
